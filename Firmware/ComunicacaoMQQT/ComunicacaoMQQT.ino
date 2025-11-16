c227e39f39cb0cb6f843f2b09e980bc27c468f4d
adr1n <andrejacintorodrigues@gmail.com>
2025-11-16 20:15:26 -0300

    Integracaoo e MQTT

--- Firmware/ComunicacaoMQQT/ComunicacaoMQQT.ino
+++ Firmware/ComunicacaoMQQT/ComunicacaoMQQT.ino
@@ -8,10 +8,18 @@ const char* password = "99634M{m";
 
 // Configurações MQTT
 const char* mqtt_server = "0.tcp.sa.ngrok.io"; // Exemplo: "broker.hivemq.com"
-const int mqtt_port = 19107;
+const int mqtt_port = 14171;
 const char* mqtt_user = "user";           // seu usuário MQTT
 const char* mqtt_pass = "password";             // sua senha MQTT
-const char* topic = "esp32.data";
+const char* data_topic = "esp32.data";
+// const char* device_id = "esp32";
+const char* status_topic = "devices/esp32/status";
+const char* trajeto_topic = "devices/esp32/trajeto";
+const char* client_topic = "devices/esp32/commands";
+
+// Controle de tempo
+unsigned long lastStatusTime = 0;
+unsigned long lastTrajetoTime = 0;
 
 // Struct Trajeto
 typedef struct Trajeto {
@@ -19,6 +27,7 @@ typedef struct Trajeto {
     char* comandosExecutados;   // idem
     int status;
     int tempo;
+    int ID;
 } Trajeto;
 
 Trajeto t;
@@ -26,6 +35,29 @@ Trajeto t;
 // Instancia WiFi e MQTT
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+// Publica o status
+void publicarStatus() {
+    StaticJsonDocument<128> doc;
+    doc["online"] = true; //alterar isto
+    doc["bateria"] = 95;  //alterar isto
+    char buffer[128];
+    size_t n = serializeJson(doc, buffer);
+    client.publish(status_topic, buffer, n);
+    Serial.println("Status publicado!");
+}
+
+// Publica o trajeto
+void publicarTrajeto(Trajeto trajeto) {
+    StaticJsonDocument<192> doc;
+    doc["idTrajeto"] = trajeto.ID;
+    doc["Executadas"] = trajeto.comandosExecutados;
+    doc["device_id"] = "esp32";
+    char buffer[192];
+    size_t n = serializeJson(doc, buffer);
+    client.publish(trajeto_topic, buffer, n);
+    Serial.println("Trajeto publicado!");
+}
 
 // Função para conectar WiFi
 void setup_wifi() {
@@ -44,7 +76,7 @@ void reconnect() {
   while (!client.connected()) {
     if (client.connect("ESP32Client", mqtt_user, mqtt_pass)) {
       Serial.printf("Conectado ao cliente!\n");
-      client.subscribe(topic);
+      client.subscribe(client_topic);
       digitalWrite(2, LOW);
       delay(100);
       digitalWrite(2, HIGH);
@@ -66,27 +98,24 @@ void callback(char* topic, byte* payload, unsigned int length) {
     if (length >= sizeof(buffer)) length = sizeof(buffer) - 1; // evita overflow
     memcpy(buffer, payload, length);
     buffer[length] = '\0'; // Termina a string corretamente
-
+    Serial.println("Foram recebidos comandos!");
     // Agora, atribua uma cópia para comandosEnviados
     t.comandosEnviados = strdup(buffer); // strdup faz cópia dinâmica
+    Serial.printf("Comando recebido e armazenado: %s\n", t.comandosEnviados);
 }
 
-// Serializa Trajeto para JSON e publica
-void sendTrajetoMQTT(Trajeto trajeto) {
-  StaticJsonDocument<512> doc;
-  JsonArray enviados = doc.createNestedArray("comandosEnviados");
-  JsonArray executados = doc.createNestedArray("comandosExecutados");
-  for (int i = 0; i < 3; i++) {
-    enviados.add(trajeto.comandosEnviados[i]);
-    executados.add(trajeto.comandosExecutados[i]);
-  }
-  doc["status"] = trajeto.status;
-  doc["tempo"] = trajeto.tempo;
+// // Serializa Trajeto para JSON e publica
+// void sendTrajetoMQTT(Trajeto trajeto) {
+//   StaticJsonDocument<512> doc;
+//   doc["comandosEnviados"] = trajeto.comandosEnviados;
+//   doc["comandosExecutados"] = trajeto.comandosExecutados;
+//   doc["status"] = trajeto.status;
+//   doc["tempo"] = trajeto.tempo;
 
-  char buffer[512];
-  size_t n = serializeJson(doc, buffer);
-  client.publish(topic, buffer, n);
-}
+//   char buffer[512];
+//   size_t n = serializeJson(doc, buffer);
+//   client.publish(data_topic, buffer, n);
+// }
 
 void setup() {
   Serial.begin(115200);
@@ -96,6 +125,14 @@ void setup() {
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
   reconnect();
+
+  t.comandosExecutados = strdup("a1219d89a0981e91a0100");
+  t.status = 1;
+  t.tempo = 120;
+  t.ID = 10;
+
+  lastStatusTime = millis();
+  lastTrajetoTime = millis();
 }
 
 void loop() {
@@ -104,12 +141,16 @@ void loop() {
   }
   client.loop();
 
-  // Exemplo de uso:
-  // Trajeto t;
-  // t.comandosEnviados = strdup("a1220da0980ea0099");            // Exemplo de string separada por vírgula
-  t.comandosExecutados = strdup("a1219d89a0981e91a0100");
-  t.status = 1;
-  t.tempo = 120;
-  sendTrajetoMQTT(t);
-  delay(5000); // envia a cada 5s
+  // Publica status a cada 30 segundos
+  if (millis() - lastStatusTime > 30000) {
+      publicarStatus();
+      lastStatusTime = millis();
+  }
+  // Publica trajeto a cada 10 segundos
+  if (millis() - lastTrajetoTime > 10000) {
+      publicarTrajeto(t);
+      lastTrajetoTime = millis();
+  }
+  // delay(3000); // envia a cada 5s
 }
+
---
+++ Firmware/Integracao/Integracao.ino
@@ -0,0 +1,144 @@
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+// #include <WiFi.h>
+#include <AGV_Eric.h>
+#include <ESP32Servo.h>
+
+// === Bluetooth ===
+#include "BluetoothSerial.h"
+
+String device_name = "ESP32-BT-Logger";
+BluetoothSerial SerialBT;
+
+// Check if Bluetooth is available
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+// Check Serial Port Profile
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+// === Definição dos pinos ===
+#define SDA_1 19
+#define SCL_1 18
+#define SDA_2 21
+#define SCL_2 22
+
+// === Wifi ===
+// // Configurações WiFi
+// const char* ssid = "ANDRE_5907";
+// const char* password = "99634M{m";
+
+// === Criação das interfaces I2C ===
+TwoWire I2C_1 = TwoWire(0);
+TwoWire I2C_2 = TwoWire(1);
+
+// === Criação dos sensores ===
+Adafruit_INA219 ina219(0x40);
+Adafruit_MPU6050 mpu6050;
+  
+// Instância dos motores e do AGV
+DCMotor Motor1, Motor2;
+AGV AGV1;
+Servo servo_1;
+uint8_t motor_angle;
+
+void setup() {
+  SerialBT.begin(device_name);  //Bluetooth device name
+  //SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+  
+  // WiFi.begin(ssid, password);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   Serial.printf("Nao foi possivel conectar-se ao wifi.\n");
+  //   delay(500);
+  // }
+  Serial.begin(115200);
+  delay(1000);
+  
+  servo_1.attach(33);
+  AGV1.Create(12, 13, 14, 27);  // Inicializando o AGV com os pinos dos motores
+
+  // Inicializando I2C_1 para o INA219
+  I2C_1.begin(SDA_1, SCL_1, 100000);
+  if (!ina219.begin(&I2C_1)) { // Endereço padrão do INA219 é 0x40
+    SerialBT.println("Não foi possível encontrar o INA219!");
+  } else {
+    SerialBT.println("INA219 detectado no barramento 1.");
+  }
+
+  // Inicializando I2C_2 para o MPU6050
+  I2C_2.begin(SDA_2, SCL_2, 100000);
+  if (!mpu6050.begin(0x68, &I2C_2)) { // Endereço padrão do MPU6050 é 0x68
+    SerialBT.println("Não foi possível encontrar o MPU6050!");
+  } else {
+    SerialBT.println("MPU6050 detectado no barramento 2.");
+  }
+
+  SerialBT.println("Pronto para leituras!\n");
+
+  //Move axis to specific positions, both limits 0, 180 and mid 90
+  servo_1.write(0);
+  Serial.println("MOVE TO POS_0");              
+  delay(3000);           
+  
+  servo_1.write(90);
+  Serial.println("MOVE TO POS_90");              
+  delay(3000);   
+  
+  servo_1.write(180);
+  Serial.println("MOVE TO POS_180");              
+  delay(3000);   
+
+}
+
+void loop() {
+
+  AGV1.ForwardAGV();
+
+  // === Leitura do INA219 ===
+  float current = ina219.getCurrent_mA();
+  float voltage = ina219.getBusVoltage_V();
+  float power = ina219.getPower_mW();
+
+  // === Leitura do MPU6050 ===
+  sensors_event_t a, g, temp;
+  mpu6050.getEvent(&a, &g, &temp);
+
+  // === Exibe os valores ===
+  SerialBT.println("===== Leitura INA219 =====");
+  SerialBT.printf("Corrente = %f mA | Tensao = %f | Potencia = %f mW\n", current, voltage, power); 
+
+  SerialBT.println("===== Leitura MPU6050 =====");
+  SerialBT.printf("Acelerômetro: X= %f m/s^2 Y= %f m/s^2 Z= %f m/s^2\n", a.acceleration.x, a.acceleration.y, a.acceleration.z); 
+
+  SerialBT.printf("Giroscópio: X= %f rad/s Y= %f rad/s Z=%f rad/s\n", g.gyro.x, g.gyro.y, g.gyro.z); 
+
+  SerialBT.printf("Temperatura: %f C\n", temp.temperature);
+
+  servo_1.write(0);
+  delay(3000);
+  servo_1.write(180);
+  delay(3000);
+  // SerialBT.println("FROM 0 to 180");  
+  // for (motor_angle = 0; motor_angle <= 180; motor_angle ++) { 
+  //   servo_1.write(motor_angle);
+  //   SerialBT.println(motor_angle);           
+  //   delay(15);                       
+  // }
+  // delay(1000);
+
+  // SerialBT.println("FROM 180 to 0"); 
+  // for (motor_angle = 180; motor_angle >= 0; motor_angle --) { 
+  //   servo_1.write(motor_angle); 
+  //   SerialBT.println(motor_angle);                
+  //   delay(15);                       
+  // }
+
+  // delay(500);
+}
+  
